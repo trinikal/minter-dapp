88cbb90025a98bd9d5f08af56a534c87af01e94e
kisspop <pentaprizmdm@gmail.com>
2022-06-07 20:44:02 -0400

    config

--- backend/src/config.js
+++ backend/src/config.js
@@ -138,7 +138,7 @@ const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the
 const PRESALE_WHITELISTED_ADDRESSES = null; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
-let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually include it
+let CONTRACT_ADDRESS = "0x372Fb5D3c950591b2853f949ae6f95C6e845F05e"; // If you want to manually include it
 
 // Generic Metadata is optional if you want to reveal your NFTs
 const GENERIC = false; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
