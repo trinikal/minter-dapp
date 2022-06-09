1efc48c760ab444df7bb0a9d7349255aff8897d9
kisspop <pentaprizmdm@gmail.com>
2022-06-09 11:31:40 -0400

    245change

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -242,7 +242,7 @@ async function loadInfo() {
   const mintDecrement = document.getElementById("mintDecrement");
   const setQtyMax = document.getElementById("setQtyMax");
   const min = mintInput.attributes.min.value || false;
-  const max = 5;
+  const max = 5 || false;
   mintDecrement.onclick = () => {
     let value = parseInt(mintInput.value) - 1 || 1;
     if(!min || value >= min) {
